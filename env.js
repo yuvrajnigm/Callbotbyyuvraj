56a206650412f578d8b37d90ea0b2ccc3618e23b
yuvrajnigm <yuvrajnigam79@gmail.com>
2026-02-20 22:41:57 +0530

    Update sensitive information in env.js

--- env.js
+++ env.js
@@ -4,7 +4,7 @@
 export const USERNAME = "tgonly712@gmail.com";
 export const PASSWORD = "Yuvraj@2008";
 
-export const BOT_TOKEN = "8582860509:AAEW6Xsnx9GKyUhYSspsFKVioctckwcTW8Q";
+export const BOT_TOKEN = "8582860509:AAFvrnCJ8JfUyq-HyUD8GEjfLNygUt3_Wd8";
 export const CHAT_ID = "-1003787254360";
 
 export const REFRESH_INTERVAL_MINUTES = 30;
@@ -16,4 +16,5 @@ export const MAIN_CHANNEL_URL = "https://t.me/YUVRAJNUMBERS";
 
 export const ADMIN_NAME = "Contact Admin";
 export const ADMIN_URL = "https://t.me/Illuminate786";
+
 
